0a6a5249220d55003fe7e55738a6761799d37762
Pavan-Kumar9381 <ramgiripavankumar@gmail.com>
2025-01-03 10:14:49 +0530

    while loop

---
+++ Demo5.java
@@ -0,0 +1,11 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        String username = "user001";
+        String pwd = "user_001";
+        while ((username == "user001") && (pwd == "user 001")) {
+            System.err.println("Hello happy new year");
+            break;
+        }
+    }
+}
+      
