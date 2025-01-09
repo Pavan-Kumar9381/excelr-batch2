32ace23057060d4febe9016c2fab3fb45e7a1b85
Pavan-Kumar9381 <ramgiripavankumar@gmail.com>
2025-01-09 11:32:19 +0530

    minutes to year and date

---
+++ Demo20.java
@@ -0,0 +1,18 @@
+public class Demo20 {
+    public static void main(String[] args) {
+      
+        long minutes = 5256000;
+
+  
+        int minutesInHour = 60;
+        int hoursInDay = 24;
+        int daysInYear = 365;
+
+        long totalDays = minutes / (minutesInHour * hoursInDay);
+        long years = totalDays / daysInYear;
+        long remainingDays = totalDays % daysInYear;
+
+
+        System.out.println(minutes + " minutes is approximately " + years + " years and " + remainingDays + " days.");
+    }
+}
